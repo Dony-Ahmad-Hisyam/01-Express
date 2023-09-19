580c8779e30cc80188dbcae6a57f921d8e2369e1
Dony-Ahmad-hisyam <donyahmad4321@gmail.com>
2023-09-19 14:58:29 +0700

    03_express

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mhsRouter = require("./routes/mahasiswa");
+const bodyPrs = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyPrs.urlencoded({ extended: false }));
+app.use(bodyPrs.json());
 
 app.use("/api/mhs", mhsRouter);
 
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { body, validationResult } = require("express-validator");
+
 const connection = require("../config/db");
 
 router.get("/", function (req, res) {
@@ -15,6 +17,33 @@ router.get("/", function (req, res) {
         status: true,
         message: "Data Mahasiswa",
         data: rows,
+      });
+    }
+  });
+});
+
+router.post("/store", [body("nama").notEmpty(), body("nrp").notEmpty()], (req, res) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      error: error.array(),
+    });
+  }
+  let Data = {
+    nama: req.body.nama,
+    nrp: req.body.nrp,
+  };
+  connection.query("INSERT into mahasiswa set ? ", Data, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(201).json({
+        status: true,
+        message: "Sukses..!",
+        data: rows[0],
       });
     }
   });
